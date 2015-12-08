5413ee263263f577a1359c7707a86aae2c602e4e
Marcin Skiba <marcinskiba91@gmail.com>
2015-12-08 11:54:13 +0100

    comments: code clean up

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,12 @@ class Comment < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   validates :comment, presence: true
+
+  before_save :add_protocol_to_website
+
+  def add_protocol_to_website
+    if website.nil? == false and website.blank? == false
+      self.website = 'http://' + website if website.index('http') != 0
+    end
+  end
 end
