d875284781fc540784e119974e7d473fc380d475
Marcin Skiba <marcinskiba91@gmail.com>
2016-02-15 23:00:03 +0100

    updated 'author' page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@ class StaticPagesController < ApplicationController
   def author
     @active_menu_item = "author"
     @page_image = "/assets/author.jpg"
-    @page_title = "Marcin Skiba"
-    @page_summary = "Hey folks! My name is Marcin, I am the CEO at Coding Buzz - a software company of my own."
+    @page_title = ""
+    @page_summary = ""
   end
 
 
